08fb0296a154ec01cb36e3e1680d555f667171bc
elsamoreno <elsa.moreno@edu.uah.es>
2026-01-21 11:14:10 +0100

    Add logo 9

--- app_project/app.py
+++ app_project/app.py
@@ -6,7 +6,7 @@ col1, col2, col3 = st.columns([1,2,1])
 with col2:
     st.image("novo_nordisk_logo.png", width=200)
 with col3:
-    st.image("logo.png", width=200)
+    st.image("logo.png", width=400)
 
 
 
